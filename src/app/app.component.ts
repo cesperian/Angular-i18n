0f4125e554f63eca612fb804ed49c939c213f720
cesperian <admin@5pow2.com>
2025-01-30 13:26:29 -0800

    Code changes for Part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import {Observable} from "rxjs";
+import {TranslateService} from "@ngx-translate/core";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    AsyncPipe
+  ],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
 
+  translateBase = [
+    $localize`:@@books.hitchhiker:The Hitchhiker's Guide to the Galaxy`,
+    $localize`:@@books.restaurant:The Restaurant at the End of the Universe`,
+    $localize`:@@books.universe:Life, the Universe and Everything`,
+    $localize`:@@books.fish:So Long, and Thanks for All the Fish`,
+    $localize`:@@books.harmless:Mostly Harmless`,
+  ]
+  translateContent$: Observable<any>;
+
+  constructor(public translate: TranslateService) {
+    this.translate.addLangs(['en-US']);
+    this.translate.setDefaultLang('en-US');
+    this.translate.use('en-US');
+    this.translateContent$ = translate.stream('books');
+  }
 }
